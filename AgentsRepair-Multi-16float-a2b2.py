863c3a378d3dd98570f47e00535c402a4afe9b88
YeWenguang <166815701+YeWenguang@users.noreply.github.com>
2024-04-13 19:23:20 +0800

    Add files via upload

--- AgentsRepair-Multi-16float-a2b2.py
+++ AgentsRepair-Multi-16float-a2b2.py
@@ -10,9 +10,10 @@ from tqdm import tqdm
 from torch.nn.parallel import DataParallel
 
 
-model1_id = "/home/yewenguang/work/codellama-7b-instrct-hf"
-model2_id = "/home/yewenguang/work/Mistral-7B-Instruct-v0.2"
-file_path = "/home/yewenguang/work/codellama-main/SPoC/train-eval-300.txt"
+model1_id = "codellama/CodeLlama-7b-Instruct-hf"
+model2_id = "mistralai/Mistral-7B-Instruct-v0.2"
+testcases_base_path = './SPoC/testcases'
+file_path = "./SPoC/train-eval-300.txt"
 
 device = "cuda"
 
@@ -104,7 +105,6 @@ def compile_and_run_cpp(cpp_code):
 
 def read_test_cases(probid):
     test_cases = []
-    testcases_base_path = '/home/yewenguang/work/Code-Llama/spoc/testcases'
     testcases_path = f"{testcases_base_path}/{probid}/{probid}_testcases_public.txt"
     print("正在进行测试用例测试：")
     print(f"testcases_path: {testcases_path}")
